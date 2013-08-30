b43517094ce697a06de7bb3ee3522720d0a8078b
andrew pate <andrewpate09@live.co.uk>
2013-08-30 20:39:58 +0100

    Added column adding methods to Table class

--- databaseCacheTest/src/databasecachetest/Table.java
+++ databaseCacheTest/src/databasecachetest/Table.java
@@ -22,7 +22,16 @@ public class Table {
 		tableWidth = 0;
 	}
 	
-	void addByteColumn(String columnName) throws FemtoDBException
+	//*******************************************************************
+	//*******************************************************************
+	//*******************************************************************
+	//                COLUMN ADDING METHODS
+	//*******************************************************************
+	//*******************************************************************
+	//*******************************************************************
+	
+	/** Adds a byte column to the table */
+	final void addByteColumn(String columnName) throws AddedColumnToOperationalTableException
 	{
 		if(operationalTable) throw new AddedColumnToOperationalTableException();
 		columnNames = addToArray(columnNames, columnName);
@@ -30,6 +39,129 @@ public class Table {
 		columnByteWidth = addToArray(columnByteWidth,1);
 		tableWidth++;
 	}
+	
+	/** Adds a Boolean column to the table, its true width is one byte */
+	final void addBooleanColumn(String columnName) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		columnByteWidth = addToArray(columnByteWidth,1);
+		tableWidth++;
+	}
+	
+	/** Adds a byte array column to the table, its true width is width + 4 bytes to encode length*/
+	final void addBytesColumn(String columnName, int width) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		int trueWidth = 4 + width;
+		columnByteWidth = addToArray(columnByteWidth,trueWidth);
+		tableWidth += trueWidth;
+	}
+	
+	/** Adds a Short column to the table, its true width is 2 bytes */
+	final void addShortColumn(String columnName) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		columnByteWidth = addToArray(columnByteWidth,2);
+		tableWidth += 2;
+	}
+	
+	/** Adds a Character column to the table, its true width is 2 bytes */
+	final void addCharColumn(String columnName) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		columnByteWidth = addToArray(columnByteWidth,2);
+		tableWidth += 2;
+	}
+	
+	/** Adds a Integer column to the table, its true width is 2 bytes */
+	final void addIntegerColumn(String columnName) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		columnByteWidth = addToArray(columnByteWidth,4);
+		tableWidth += 4;
+	}
+	
+	/** Adds a Long column to the table, its true width is 2 bytes */
+	final void addLongColumn(String columnName) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		columnByteWidth = addToArray(columnByteWidth,8);
+		tableWidth += 8;
+	}
+	
+	/** Adds a Float column to the table, its true width is 2 bytes */
+	final void addFloatColumn(String columnName) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		columnByteWidth = addToArray(columnByteWidth,4);
+		tableWidth += 4;
+	}
+	
+	/** Adds a Double column to the table, its true width is 2 bytes */
+	final void addDoubleColumn(String columnName) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		columnByteWidth = addToArray(columnByteWidth,8);
+		tableWidth += 8;
+	}
+	
+	/** Adds a Char array column to the table, its true width is width + 2 bytes to encode length*/
+	final void addCharsColumn(String columnName, int width) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		int trueWidth = 2 + width;
+		columnByteWidth = addToArray(columnByteWidth,trueWidth);
+		tableWidth += trueWidth;
+	}
+	
+	/** Adds a String column to the table, its true width is width + 2 bytes, to encode the length. It is important to note the string gets stored in modified UTF format so the available width in characters may be less than the width parameter */
+	final void addStringColumn(String columnName, int width) throws AddedColumnToOperationalTableException
+	{
+		if(operationalTable) throw new AddedColumnToOperationalTableException();
+		columnNames = addToArray(columnNames, columnName);
+		columnByteOffset = addToArray(columnByteOffset,tableWidth);
+		int trueWidth = 2 + width;
+		columnByteWidth = addToArray(columnByteWidth,trueWidth);
+		tableWidth += trueWidth;
+	}
+	
+	//*******************************************************************
+	//*******************************************************************
+	//*******************************************************************
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	private int[] addToArray(int[] in, int toAdd)
 	{
